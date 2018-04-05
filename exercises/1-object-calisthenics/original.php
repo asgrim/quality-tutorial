806ce28bccc52b4d84e2136240264e7980f26ff6
James Titcumb <james@asgrim.com>
2018-04-05 13:27:53 +0100

    Object calisthenics exercise

---
+++ exercises/1-object-calisthenics/original.php
@@ -0,0 +1,96 @@
+<?php
+declare(strict_types=1);
+
+namespace Exercises;
+
+use Assert\Assert;
+
+interface HttpClient
+{
+    public function request(string $url, string $method, string $body) : int;
+    public function getLastResponse(): string;
+}
+
+class PaymentDetails
+{
+    private $tid;
+    private $amt;
+    private $isPaid;
+    public function getTid()
+    {
+        return $this->tid;
+    }
+    public function setTid(string $tid)
+    {
+        $this->tid = $tid;
+    }
+    public function getAmt()
+    {
+        return $this->amt;
+    }
+    public function setAmt(float $amt)
+    {
+        $this->amt = $amt;
+    }
+    public function getisPaid()
+    {
+        return $this->isPaid;
+    }
+    public function setIsPaid($isPaid)
+    {
+        $this->isPaid = $isPaid;
+    }
+}
+
+class MyFunPaymentGateway
+{
+    /** @var HttpClient */
+    private $http;
+
+    /** @var string[] */
+    private $responses;
+
+    public function __construct(HttpClient $http)
+    {
+        $this->http = $http;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws \Exception
+     */
+    public function preauthorise(PaymentDetails $paymentDetails)
+    {
+        $setupResult = $this->http->request('/setup-transaction', 'POST', json_encode([
+            'tid' => $paymentDetails->getTid(),
+            'amt' => $paymentDetails->getAmt(),
+        ]));
+        $this->responses[] = json_decode($this->http->getLastResponse(), true);
+        if ($setupResult === 200) {
+            $preauthResult = $this->http->request('/preauthorise', 'POST', $paymentDetails->getTid());
+            $responses[] = json_decode($this->http->getLastResponse(), true);
+            if ($preauthResult === 200) {
+                $paymentDetails->setIsPaid(true);
+                return true;
+            } else {
+                throw new \Exception('Unable to do things');
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function getBookingId()
+    {
+        foreach ($this->responses as $response) {
+            if (isset($response['booking_id'])) {
+                return $response['booking_id'];
+            }
+        }
+    }
+
+    public function completePayment(string $pmt)
+    {
+        // TODO: Implement completePayment() method.
+    }
+}
---
+++ handbook/1-object-calisthenics/notes.md
@@ -0,0 +1,13 @@
+# Background knowledge
+
+ - `HttpClient` is just a placeholder for guzzle or something
+ - Indentation / avoiding `else` combined
+
+# Things to improve
+
+ - Return-early for error conditions
+ - `float` for money - BAD! - definitely wrap that up
+ - Naming: what is `tid`? - don't abbreviate
+ - `setIsPaid` - hidden away inside: ASK don't tell
+ - getters / setters: TELL don't ASK
+ - Consider inverting call stack; `PaymentDetails` calls `MyFunPaymentGateway`
